45538cf43b0e1016c81c12114d6dad79b39f9f0f
Swansen1993 <niederloehnersven@gmail.com>
2026-05-27 15:30:20 +0200

    Initial Commit

---
+++ Alarm_App/Alarm_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alarm_AppApp.swift
+//  Alarm_App
+//
+//  Created by Sven Niederlöhner on 27.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Alarm_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
